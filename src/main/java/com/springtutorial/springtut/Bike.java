836565e31315c760eecec241d7f4cfd5825957fb
Nantha <nanthu1593@gmail.com>
2024-04-13 15:07:11 +0800

    comp added

--- src/main/java/com/springtutorial/springtut/Bike.java
+++ src/main/java/com/springtutorial/springtut/Bike.java
@@ -1,5 +1,8 @@
 package com.springtutorial.springtut;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Bike implements vehicle{
 	public void drive() {
 		System.out.println("Bike Engine ON");
