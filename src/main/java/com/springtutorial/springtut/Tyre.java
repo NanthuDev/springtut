11416ae4cb990b3067c74dac6f16e9adde48f8ee
Nantha <nanthu1593@gmail.com>
2024-04-13 17:31:24 +0800

    Update Tyre.java

--- src/main/java/com/springtutorial/springtut/Tyre.java
+++ src/main/java/com/springtutorial/springtut/Tyre.java
@@ -1,7 +1,18 @@
 package com.springtutorial.springtut;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Tyre {
  private String brand;
+
+ 
+//public Tyre(String brand) {
+	//super();
+	//this.brand = brand;
+//}
+
+
 
 public String getBrand() {
 	return brand;
@@ -13,6 +24,7 @@ public void setBrand(String brand) {
 
 @Override
 public String toString() {
-	return "Tyre [brand=" + brand + "]";
+	//return "Tyre [brand=" + brand + "]";
+	return "Running in TYRE";
 }
 }
