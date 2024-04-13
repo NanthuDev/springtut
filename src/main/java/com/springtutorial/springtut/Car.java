c9abf7b266162a982ca70e92410d736e8d5520ed
Nantha <nanthu1593@gmail.com>
2024-04-13 17:30:22 +0800

    Update Car.java

--- src/main/java/com/springtutorial/springtut/Car.java
+++ src/main/java/com/springtutorial/springtut/Car.java
@@ -1,10 +1,26 @@
 package com.springtutorial.springtut;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements vehicle {
+	
+	@Autowired
+	private Tyre tyre;
+	
+	
+	public Tyre getTyre() {
+		return tyre;
+	}
+
+
+	public void setTyre(Tyre tyre) {
+		this.tyre = tyre;
+	}
+
+
 	public void drive() {
-		System.out.println("Engine ON");
+		System.out.println("Engine ON " + tyre);
 	}
 }
